c9b90279ffd57334caeb9ea74670942311b1f449
kryptokommunist <marcusding@icloud.com>
2017-03-25 04:20:31 +0100

    add diffing of panels for faster rendering and add pixel drawing

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -19,62 +19,134 @@ void setup() {
   board->init();
 }
 
-uint16_t framebuffer[25] = {0,0b0000001110000000,0b0000011111000000,0b0000111111100000,0b0001111111110000,0b0011111111111000,0b0011111111111000,0b0011111111111000,
-0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,0b0011111111111000,
-0b0011111111111000,0b0001111111110000,0b0000111111100000,0b0000011111000000,0b0000001110000000,0};
+uint16_t framebuffer[25] = {0, 0b0000001110000000, 0b0000011111000000, 0b0000111111100000, 0b0001111111110000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000,
+                            0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000,
+                            0b0011111111111000, 0b0001111111110000, 0b0000111111100000, 0b0000011111000000, 0b0000001110000000, 0
+                           };
 
-uint16_t framebuffer2[25] = {0,0,0,0,0,0b1111111111111111,0b1111111111111111,0b1111111111111111,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b0000001110000000,0b1111111111111111,0b1111111111111111,0b1111111111111111,0,0,0,0,0};
+uint16_t framebuffer2[25] = {0, 0, 0, 0, 0, 0b1111111111111111, 0b1111111111111111, 0b1111111111111111, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b1111111111111111, 0b1111111111111111, 0b1111111111111111, 0, 0, 0, 0, 0};
 
 void loop() {
-board->render_string_small("Hello World!\0",0);
-delay(1000);
-board->render_string_small("Hi biatch!\0",0);
-delay(2000);
-board->render_string_small("12:34\0",0);
-delay(2000);
-board->render_string_small("12:35\0",0,3);
-delay(2000);
-board->render_string_small("23:49\0",0,10);
-delay(2000);
-board->render_string_small("23:49\0",0,-10);
-delay(2000);
-board->render_string_small("23:49\0",0,-4);
-delay(2000);
-board->render_string_small("23:49\0",0,-2);
-delay(2000);
-board->render_string_small("23:49\0",0,10);
-delay(2000);
-board->render_string_small("23:49\0",0,13);
-delay(2000);
-board->render_string_small("23:49\0",0,8);
-delay(2000);
-for(int i = 0; i < 20; i++) {
+  board->render_string("HI IHR SPASTEN\0", 4);
+  delay(1000);
+  board->all_off();
+  int h = 0;
+  const float delta = 8 * PI/115;
+  for (float a = 0; a <= 8 * PI; a += delta) {
+    board->draw_in_internal_framebuffer(1,h++,8*(1+sin(a)));
+    board->render_internal_framebuffer();
+    delay(10);
+  }
+  board->all_off();
+  delay(1000);
+  board->all_on();
+  delay(1000);
+  board->all_off();
+  for (int i = 0; i < 2; i++) {
+    board->all_on();
+    board->all_off();
+  }
+  delay(1000);
+  for (float a = 0; a <= 2 * PI; a += 0.2) {
+    int x = 1 + 8 * cos(a);
+    int y = 4 + 8 * sin(a);
+    board->render_string_small("12:34\0", x, y);
+  }
+  for (float a = 2 * PI; a >= 0; a -= 0.2) {
+    int x = 1 + 8 * cos(a);
+    int y = 4 + 8 * sin(a);
+    board->render_string_small("12:34\0", x, y);
+  }
+  board->render_string_small("12:34\0", 0, 4);
+  delay(500);
+  board->render_string_small("12 34\0", 0, 4);
+  delay(500);
+  board->render_string_small("12:35\0", 0, 4);
+  delay(500);
+  board->render_string_small("12 35\0", 0, 4);
+  delay(500);
+  board->render_string_small("12:36\0", 0, 4);
+  delay(500);
+  board->render_string_small("12 36\0", 0, 4);
+  delay(1000);
+  board->render_string("0\0", 8);
+  delay(1000);
+  board->render_string("1\0", 8);
+  delay(1000);
+  board->render_string("2\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("3\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("4\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("5\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("6\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("7\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("8\0", 8);
+  delay(1000);
+  board->all_off();
+  board->render_string("9\0", 8);
+  delay(1000);
+  board->render_string("10\0", 4);
+  delay(1500);
+  board->all_off();
+  board->scroll_string("FlipDot>LUV");
+  delay(1000);
+
+  /*
+    for(int  x= -4; x < 18; x++) {
+     for(int y = -7; y < 16; y++) {
+     board->render_string_small("12:34\0",x,y);
+    }
+    }
+    for(int y = -7; y < 16; y++) {
+    for(int  x= -4; x < 18; x++) {
+     board->render_string_small("12:34\0",x,y);
+    }
+    }
+  */
+
+  board->render_string_small("12:34\0");
+  delay(2000);
+  board->render_string_small("23:49\0", 0, 4);
+  delay(2000);
+  board->render_string_small("23:50\0", 0, 4);
+  delay(2000);
+  board->render_string_small("23:49\0", 0, 8);
+  delay(2000);
+  for (int i = 0; i < 20; i++) {
     board->render_frame(framebuffer);
-    delay(i*5);
+    delay(i * 5);
     board->render_frame(framebuffer2);
-    delay(i*5);
-}
-board->scroll_string("Hello World!\0",40);
-delay(500);
-board->scroll_string("I LOVE FLIP-DOTS MORE THAN $$!!\0",0);
-delay(1000);
-board->render_string("13:42",2);
-delay(1000);
-board->all_off();
-delay(100);
-board->render_string("13:43",2);
-delay(1000);
-board->all_off();
-delay(100);
-board->render_string("13:43",2);
-delay(1000);
-board->all_off();
-delay(100);
-board->render_string("12:00",2);
-delay(1000);
-board->all_off();
-delay(100);
-board->render_string("66:00",2);
-delay(1000);
+    delay(i * 5);
+  }
+  delay(1000);
+  board->render_string("13:42", 2);
+  delay(1000);
+  board->all_off();
+  delay(100);
+  board->render_string("13:43", 2);
+  delay(1000);
+  board->all_off();
+  delay(100);
+  board->render_string("13:43", 2);
+  delay(1000);
+  board->all_off();
+  delay(100);
+  board->render_string("12:00", 2);
+  delay(1000);
+  board->all_off();
+  delay(100);
+  board->render_string("66:00", 2);
+  delay(1000);
 }
 
