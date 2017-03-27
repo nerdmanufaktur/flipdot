e656ec32e70f4fafebc2980ca6e665f13b4c9f97
kryptokommunist <marcusding@icloud.com>
2017-03-27 19:18:01 +0200

    add libraries for more efficient time tracking via ntp

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -10,29 +10,14 @@
 // Documentation and samples are available at https://github.com/nerdmanufaktur/flipdot
 // - - - -
 #include <ESP8266WiFi.h>
-#include "FLIPDOT.h"
+#include <FLIPDOT.h>
+#include "ESPTime.h"
 
 const char* ssid = "Mettigel24.de | Get off my LAN";
 const char* password = "N00bznet?NoWay!";
 
 FLIPDOT *board = new FLIPDOT();
-
-//NTC variables
-#include <WiFiUdp.h>
-unsigned int localPort = 2390;      // local port to listen for UDP packets
-
-/* Don't hardwire the IP address or we won't get the benefits of the pool.
- *  Lookup the IP address for the host name instead */
-//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
-IPAddress timeServerIP; // time.nist.gov NTP server address
-const char* ntpServerName = "time.nist.gov";
-
-const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
-
-byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
-
-// A UDP instance to let us send and receive packets over UDP
-WiFiUDP udp;
+ESPTime timer = ESPTime();
 
 void setup() {
   board->init(); //must come first to avoid error state of board
@@ -45,11 +30,6 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
-
-  Serial.println("Starting UDP");
-  udp.begin(localPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
 }
 
 uint16_t framebuffer[25] = {0, 0b0000001110000000, 0b0000011111000000, 0b0000111111100000, 0b0001111111110000, 0b0011111111111000, 0b0011111111111000, 0b0011111111111000,
@@ -60,9 +40,19 @@ uint16_t framebuffer[25] = {0, 0b0000001110000000, 0b0000011111000000, 0b0000111
 uint16_t framebuffer2[25] = {0, 0, 0, 0, 0, 0b1111111111111111, 0b1111111111111111, 0b1111111111111111, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b0000001110000000, 0b1111111111111111, 0b1111111111111111, 0b1111111111111111, 0, 0, 0, 0, 0};
 
 void loop() {
-  board->render_string(getNTPTime().c_str(), 4, ZERO_ALL);
-  delay(2000);
-  board->render_string(getNTPTime().c_str(), 4,ZERO_ALL);
+  board->render_string(timer.getFormattedTime(), 4, ZERO_ALL);
+  delay(1000);
+    board->all_off();
+  board->render_string(timer.getFormattedTime(), 4, ZERO_ALL);
+  delay(1000);
+    board->all_off();
+  board->render_string(timer.getFormattedTime(), 4, ZERO_ALL);
+  delay(1000);
+    board->all_off();
+  board->render_string(timer.getFormattedTime(), 4, ZERO_ALL);
+  delay(1000);
+    board->all_off();
+  board->render_string(timer.getFormattedTime(), 4, ZERO_ALL);
   delay(1000);
   /*
   board->all_off();
@@ -222,86 +212,3 @@ float deg = 0;
   delay(1000);
   
 }
-
-// send an NTP request to the time server at the given address
-unsigned long sendNTPpacket(IPAddress& address)
-{
-  Serial.println("sending NTP packet...");
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:
-  udp.beginPacket(address, 123); //NTP requests are to port 123
-  udp.write(packetBuffer, NTP_PACKET_SIZE);
-  udp.endPacket();
-}
-
-//get the ntc time string
-String getNTPTime(){
-  String time = "";
-  //get a random server from the pool
-  WiFi.hostByName(ntpServerName, timeServerIP); 
-
-  sendNTPpacket(timeServerIP); // send an NTP packet to a time server
-  // wait to see if a reply is available
-  delay(1000);
-  
-  int cb = udp.parsePacket();
-  if (!cb) {
-    //Serial.println("no packet yet");
-  }
-  else {
-    //Serial.print("packet received, length=");
-    //Serial.println(cb);
-    // We've received a packet, read the data from it
-    udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
-
-    //the timestamp starts at byte 40 of the received packet and is four bytes,
-    // or two words, long. First, esxtract the two words:
-
-    unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
-    unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
-    // combine the four bytes (two words) into a long integer
-    // this is NTP time (seconds since Jan 1 1900):
-    unsigned long secsSince1900 = highWord << 16 | lowWord;
-
-    // now convert NTP time into everyday time:
-    // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
-    const unsigned long seventyYears = 2208988800UL;
-    // subtract seventy years:
-    unsigned long epoch = secsSince1900 - seventyYears;
-
-    // print the hour, minute and second:
-    // UTC is the time at Greenwich Meridian (GMT)
-    time += (epoch  % 86400L) / 3600; // print the hour (86400 equals secs per day)
-    time += ':';
-    if ( ((epoch % 3600) / 60) < 10 ) {
-      // In the first 10 minutes of each hour, we'll want a leading '0'
-      time += '0';
-    }
-    time+=(epoch  % 3600) / 60; // print the minute (3600 equals secs per minute)
-    /*
-    Serial.print(':');
-    if ( (epoch % 60) < 10 ) {
-      // In the first 10 seconds of each minute, we'll want a leading '0'
-      Serial.print('0');
-    }
-    Serial.println(epoch % 60); // print the second
-    */
-    return time;
-  }
-  return "No response";
-}
-
